db83595963ec07e1287919a409f063a58a54e29f
MD.MAHMUDUL HASAN <126802456+andmahmud@users.noreply.github.com>
2026-07-05 13:38:01 +0600

    update id

--- lib/ads/ad_ids.dart
+++ lib/ads/ad_ids.dart
@@ -6,7 +6,7 @@ class AdIds {
 
   // ---------------- BANNER ----------------
   static String get banner {
-    if (kReleaseMode) return 'ca-app-pub-9067387128330174/8067968951';
+    if (kReleaseMode) return 'ca-app-pub-1195883693665145/8169196259';
     return Platform.isAndroid
         ? 'ca-app-pub-3940256099942544/6300978111'
         : 'ca-app-pub-3940256099942544/2934735716';
@@ -14,7 +14,7 @@ class AdIds {
 
   // ---------------- INTERSTITIAL ----------------
   static String get interstitial {
-    if (kReleaseMode) return 'ca-app-pub-9067387128330174/4891141274';
+    if (kReleaseMode) return 'ca-app-pub-1195883693665145/9668191741';
     return Platform.isAndroid
         ? 'ca-app-pub-3940256099942544/1033173712'
         : 'ca-app-pub-3940256099942544/4411468910';
@@ -22,9 +22,17 @@ class AdIds {
 
   // ---------------- APP OPEN ----------------
   static String get appOpen {
-    if (kReleaseMode) return 'ca-app-pub-9067387128330174/1176428408';
+    if (kReleaseMode) return 'ca-app-pub-1195883693665145/5543032919';
     return Platform.isAndroid
         ? 'ca-app-pub-3940256099942544/9257395921'
         : 'ca-app-pub-3940256099942544/5575463023';
+  }
+
+  // ---------------- REWARDED ----------------
+  static String get rewarded {
+    if (kReleaseMode) return 'ca-app-pub-1195883693665145/9288048250';
+    return Platform.isAndroid
+        ? 'ca-app-pub-3940256099942544/5224354917'
+        : 'ca-app-pub-3940256099942544/1712485313';
   }
 }
